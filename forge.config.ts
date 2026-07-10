3078e788dfd8ca5c4874c03be5f35df6757b2667
Kundan Kumar <39196236+Nukealbert@users.noreply.github.com>
2026-07-11 00:07:29 +0530

    fix(packaging): preserve node_modules and unpack native modules during packaging

--- forge.config.ts
+++ forge.config.ts
@@ -6,10 +6,16 @@ import { MakerRpm } from '@electron-forge/maker-rpm';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    ignore: (path) => {
+      if (path === '') return false;
+      const include = ['/.vite', '/node_modules', '/package.json'];
+      return !include.some(i => path.startsWith(i)) || path.includes('.bin');
+    },
   },
   rebuildConfig: {},
   makers: [
@@ -19,6 +25,7 @@ const config: ForgeConfig = {
     new MakerDeb({}),
   ],
   plugins: [
+    new AutoUnpackNativesPlugin({}),
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
       // If you are familiar with Vite configuration, it will look really familiar.
